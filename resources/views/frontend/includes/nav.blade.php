714eb316d25550a7e8bf276685d32a2766bfebcb
Nazhan Nasir <nazhannsr@gmail.com>
2020-10-25 15:05:39 +0800

    Initial Commit

---
+++ resources/views/frontend/includes/nav.blade.php
@@ -0,0 +1,20 @@
+<div>
+    @if (Route::has('login'))
+        <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
+            @auth
+                <a href="{{ url('/home') }}" class="text-sm text-gray-700 underline">Home</a>
+                <form action="{{route('logout')}}" method="post">
+                    @csrf
+                    <button type="submit" class="ml-4 text-sm text-gray-700 underline">Logout</button>
+                </form>
+            @else
+                <a href="{{ route('login') }}" class="text-sm text-gray-700 underline">Login</a>
+
+                @if (Route::has('register'))
+                    <a href="{{ route('register') }}" class="ml-4 text-sm text-gray-700 underline">Register</a>
+                @endif
+            @endif
+        </div>
+    @endif
+</div>
+
